344a855a45d8f53df79995863e44246ab02344f1
higuruchi <fumiya2324@gmail.com>
2021-11-16 21:21:18 +0900

    起動した際にメッセージを表示するように修正した

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/higuruchi/participant-app/internal/di"
 )
 
 func main() {
+	fmt.Println("Start participant-app on 1323!")
 	server, f := di.InitializeServer()
 	server.Run()
 	f()
