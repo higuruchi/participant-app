6d2ac5c3ac59f3406f54efc161360095b35c5a62
higuruchi <fumiya2324@gmail.com>
2021-11-16 21:20:20 +0900

    学年を判別する関数をEntityに移動した

--- internal/entity/participant_entity.go
+++ internal/entity/participant_entity.go
@@ -1,6 +1,9 @@
 package entity
 
 import (
+	"fmt"
+	"time"
+	"strconv"
 	"errors"
 )
 
@@ -12,6 +15,7 @@ type participantEntity struct {
 type ParticipantEntity interface {
 	GetID() string
 	GetName() string
+	DistinguishGrade() (string, error)
 }
 
 func NewParticipantEntity(id string, name string) (ParticipantEntity ,error) {
@@ -32,4 +36,44 @@ func (participantEntity *participantEntity) GetID() string {
 
 func (participantEntity *participantEntity) GetName() string {
 	return participantEntity.name
+}
+
+// この部分汚いw
+func (participantEntity *participantEntity) DistinguishGrade() (string, error) {
+	t := time.Now()
+	id := participantEntity.id;
+	nowYear := t.Year()
+	admissionYear, err := strconv.Atoi(fmt.Sprintf("%d%s", 20, id[0:2]))
+	if err != nil {
+		return "", fmt.Errorf("calling strconv.Atoi: %w", err)
+	}
+	// 現在Gの学籍番号には対応していない
+	// if id[2] == "G" {
+	// 	switch nowYear-admissionYear {
+	// 	case 0:
+	// 		return "M1", nil
+	// 	case 1:
+	// 		return "M2", nil
+	// 	default:
+	// 		return "", errors.New("invalid year")
+
+	// 	}
+	// }
+
+	switch nowYear-admissionYear {
+	case 0:
+		return "B1", nil
+	case 1:
+		return "B2", nil
+	case 2:
+		return "B3", nil
+	case 3:
+		return "B4", nil
+	case 4:
+		return "M1", nil
+	case 5:
+		return "M2", nil
+	default:
+		return "", errors.New("invalid year")
+	}
 }
--- internal/usecase/participants_usecase.go
+++ internal/usecase/participants_usecase.go
@@ -2,11 +2,8 @@ package usecase
 
 import (
 	"fmt"
-	"time"
 	"errors"
-	"strconv"
 	"github.com/higuruchi/participant-app/internal/entity"
-	// "github.com/higuruchi/participant-app/internal/usecase/model"
 	"github.com/higuruchi/participant-app/internal/usecase/repository"
 )
 
@@ -61,7 +58,7 @@ func (participantsUsecase *participantsUsecase) GetParticipants(
 			return nil, fmt.Errorf("calling NewParticipantEntity: %w", err)
 		}
 
-		grade, err := distinguishGrade(participantEntity.GetID())
+		grade, err := participantEntity.DistinguishGrade()
 		if err != nil {
 			return nil, fmt.Errorf("calling distinguishGrade: %w", err)
 		}
@@ -70,43 +67,4 @@ func (participantsUsecase *participantsUsecase) GetParticipants(
 	}
 
 	return participantsEntity, nil	
-}
-
-// この部分汚いw
-func distinguishGrade(id string) (string, error) {
-	t := time.Now()
-	nowYear := t.Year()
-	admissionYear, err := strconv.Atoi(fmt.Sprintf("%d%s", 20, id[0:2]))
-	if err != nil {
-		return "", fmt.Errorf("calling strconv.Atoi: %w", err)
-	}
-	// 現在Gの学籍番号には対応していない
-	// if id[2] == "G" {
-	// 	switch nowYear-admissionYear {
-	// 	case 0:
-	// 		return "M1", nil
-	// 	case 1:
-	// 		return "M2", nil
-	// 	default:
-	// 		return "", errors.New("invalid year")
-
-	// 	}
-	// }
-
-	switch nowYear-admissionYear {
-	case 0:
-		return "B1", nil
-	case 1:
-		return "B2", nil
-	case 2:
-		return "B3", nil
-	case 3:
-		return "B4", nil
-	case 4:
-		return "M1", nil
-	case 5:
-		return "M2", nil
-	default:
-		return "", errors.New("invalid year")
-	}
 }
